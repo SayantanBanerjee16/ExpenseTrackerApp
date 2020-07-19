62a9e6fc016929c7023618dd4fd5ddcee9dabfd8
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-07-19 17:55:18 +0530

    List set up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:expense_app/main.dart';
+import 'file:///D:/IIITM%20STUDIES/ANDROID%20DEV/Flutter%20Projects/Expense/expense_app/lib/widgets/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
